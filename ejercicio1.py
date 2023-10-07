1adf4cd3f262eff5a909db0551d7eb57d370696d
KingOfShit112 <126219773+KingOfShit112@users.noreply.github.com>
2023-10-06 19:32:44 -0600

    ejecucion del for

--- ejercicio1.py
+++ ejercicio1.py
@@ -2,14 +2,20 @@
 x = 0
 
 
-lisraInpar=[]
+listaImpar=[]
 
 while x == 0:
     numero = int(input('escriba un numero positivo: '))
         
-    if numero <= 0:
-        x = 0
-    elif numero > 0:
-        x == 1
+    if numero > 0:
         for x in range(1,numero):
+            resultado = 0
+            resultado = x % 2
+
+        
+        x = 1
+
+print(listaImpar)
+    
+
             
