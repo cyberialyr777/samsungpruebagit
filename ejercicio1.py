04faccf5013a99e77e64daf16547217198128804
Harry Hernandez Arias <132928917+HarryHndz@users.noreply.github.com>
2023-10-06 19:15:41 -0600

    for

--- ejercicio1.py
+++ ejercicio1.py
@@ -6,7 +6,9 @@ lisraInpar=[]
 
 while x == 0:
     numero = int(input('escriba un numero positivo: '))
-
+    
+    for x in range(1,numero):
+        
     if numero <= 0:
         x = 0
     elif numero > 0:
