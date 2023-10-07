696877c81754b009107fa79d5a581805aac4d5d8
Maria Fernanda <marifer160204@gmail.com>
2023-10-06 20:36:46 -0600

    codigo

--- ejrcicio2.py
+++ ejrcicio2.py
@@ -1,5 +1,3 @@
-import random as rd
-
 listan = []
 
 cantidad = int(input("Ingrese la cantidad de números de la lista: "))
@@ -8,11 +6,14 @@ for x in range(cantidad):
     valor = int(input("Agregue número: "))
     listan.append(valor)
 
+
 def calcular_media(listan):
     suma= 0
     for i in listan:
         x = listan[i]
         suma += x
     resultado = suma / len(listan)
+
+    print(resultado)
 
 calcular_media(listan)
