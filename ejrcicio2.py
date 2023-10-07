14ec1c3d6ec237ccd9eb1fc1bcb29d198258b42d
Maria Fernanda <marifer160204@gmail.com>
2023-10-06 20:21:24 -0600

    media ejer

--- ejrcicio2.py
+++ ejrcicio2.py
@@ -1,7 +1,18 @@
 import random as rd
 
-def calcular_media(lista):
+listan = []
+
+cantidad = int(input("Ingrese la cantidad de números de la lista: "))
+
+for x in range(cantidad):
+    valor = int(input("Agregue número: "))
+    listan.append(valor)
+
+def calcular_media(listan):
     suma= 0
-    for x in range(0,lista):
+    for i in listan:
+        x = listan[i]
         suma += x
-        
+    resultado = suma / len(listan)
+
+calcular_media(listan)
