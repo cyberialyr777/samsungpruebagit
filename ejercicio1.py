5062836cc1fd7373252dac9f1b577446286a5d57
Maria Fernanda <marifer160204@gmail.com>
2023-10-06 19:18:19 -0600

    lista

--- ejercicio1.py
+++ ejercicio1.py
@@ -6,10 +6,10 @@ lisraInpar=[]
 
 while x == 0:
     numero = int(input('escriba un numero positivo: '))
-    
-    for x in range(1,numero):
         
     if numero <= 0:
         x = 0
     elif numero > 0:
         x == 1
+        for x in range(1,numero):
+            lisraInpar.append(numero)
